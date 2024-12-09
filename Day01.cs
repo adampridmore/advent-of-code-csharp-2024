b4936940857c9dd3b5a1764045c0601bde7dd00e
adampridmore <320922+adampridmore@users.noreply.github.com>
2024-12-09 10:32:19 +0000

    Added test for day 1

---
+++ Day01.cs
@@ -0,0 +1,12 @@
+namespace advent_of_code_scala_2024;
+
+public class Day01
+{
+    private string _name = "Dave";
+
+    [Fact]
+    public void Test1()
+    {
+        Assert.Equal("Dave", _name);
+    }
+}
