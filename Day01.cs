c2dbf50ad6c9a4dd7ff91b859f87952bbb07725e
adampridmore <320922+adampridmore@users.noreply.github.com>
2024-12-09 10:57:08 +0000

    Day 1 solution

--- Day01.cs
+++ Day01.cs
@@ -6,7 +6,7 @@ namespace advent_of_code_scala_2024;
 
 public class Day01
 {
-    //private readonly string _inputFilename = @"../../../Day01_input.txt";
+    private readonly string _inputFilename = @"../../../Day01_input.txt";
 
     private readonly string _testLines = @"3   4
 4   3
@@ -23,15 +23,18 @@ public class Day01
         return new Line(int.Parse(split[0]), int.Parse(split[1]));
     }
 
-    private int CalculateTotalDistance(IEnumerable<Line> lines){
-        return 0;
+    private static int CalculateTotalDistance(IEnumerable<Line> lines){
+        var leftOrdered = lines.OrderBy(x=>x.First);
+        var rightOrdered = lines.OrderBy(x=>x.Second);
+
+        return leftOrdered
+                .Zip(rightOrdered, (a,b) => Math.Abs(a.First - b.Second))
+                .Sum();
     }
 
     [Fact]
-    public void Test1()
+    public void ExampleLines()
     {
-        // var filelines = File.ReadLines(_inputFilename);
-
         var lines = _testLines.Split(System.Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
 
         var parsedLines = lines
@@ -39,15 +42,22 @@ public class Day01
 
         Assert.Equal(new Line(3,4), parsedLines.First());
 
-        var leftOrdered = parsedLines.OrderBy(x=>x.First);
-        var rightOrdered = parsedLines.OrderBy(x=>x.Second);
-
-        var ans = 
-            leftOrdered
-                .Zip(rightOrdered, (a,b) => Math.Abs(a.First - b.Second))
-                .Sum();
+        var ans = CalculateTotalDistance(parsedLines);
 
         Assert.Equal(11, ans);
+    }
+
+    [Fact]
+    public void RealData()
+    {
+        var lines = File.ReadLines(_inputFilename);
+
+        var parsedLines = lines
+            .Select(ParseLine);
+
+        var ans = CalculateTotalDistance(parsedLines);
+
+        Assert.Equal(1938424, ans);
     }
 }
  
