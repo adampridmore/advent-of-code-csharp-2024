c3f76804d51160cb16e8f5da152c41dff6637359
adampridmore <320922+adampridmore@users.noreply.github.com>
2024-12-17 13:07:28 +0000

    Split test and implementation

--- Day03.cs
+++ Day03.cs
@@ -5,12 +5,6 @@ namespace advent_of_code_csharp_2024;
 
 public class Day03
 {
-  private static string _testInput = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
-
-  private static string _testInput_Part2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
-
-  public static readonly string InputFilename = @"../../../Day03_input.txt";
-
   private static int ProcessMul(Match m){
     var x = int.Parse(m.Groups[1].Value);
     var y = int.Parse(m.Groups[2].Value);
@@ -50,33 +44,5 @@ public class Day03
     }
 
     return total;
-  }
-
-  [Fact]
-  public void TestExample_part1(){
-    var ans = RunProgram(_testInput);
-
-    Assert.Equal(161,ans);
-  }
-
-  [Fact]
-  public void ReadData_part1(){
-    var ans = RunProgram(File.ReadAllText(InputFilename));
-
-    Assert.Equal(159833790,ans);
-  }
-
-  [Fact]
-  public void TestExample_part2(){
-    var ans = RunProgramPartII(_testInput_Part2);
-
-    Assert.Equal(48, ans);
-  }
-
-  [Fact]
-  public void RealData_part2(){
-    var ans = RunProgramPartII(File.ReadAllText(InputFilename));
-
-    Assert.Equal(1, ans);
   }
 }
---
+++ Day03Tests.cs
@@ -0,0 +1,39 @@
+namespace advent_of_code_csharp_2024;
+
+public class Day03Tests
+{
+  private static string _testInput = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+
+  private static string _testInput_Part2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+
+  public static readonly string InputFilename = @"../../../Day03_input.txt";
+
+
+  [Fact]
+  public void TestExample_part1(){
+    var ans = Day03.RunProgram(_testInput);
+
+    Assert.Equal(161,ans);
+  }
+
+  [Fact]
+  public void ReadData_part1(){
+    var ans = Day03.RunProgram(File.ReadAllText(InputFilename));
+
+    Assert.Equal(159833790,ans);
+  }
+
+  [Fact]
+  public void TestExample_part2(){
+    var ans = Day03.RunProgramPartII(_testInput_Part2);
+
+    Assert.Equal(48, ans);
+  }
+
+  [Fact]
+  public void RealData_part2(){
+    var ans = Day03.RunProgramPartII(File.ReadAllText(InputFilename));
+
+    Assert.Equal(89349241, ans);
+  }
+}
