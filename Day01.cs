9ffd3f7c27fb64919d8131675884baea58a5a1eb
adampridmore <320922+adampridmore@users.noreply.github.com>
2024-12-17 10:14:43 +0000

    Split test and implementation

--- Day01.cs
+++ Day01.cs
@@ -1,14 +1,8 @@
-using System.IO.Compression;
-using System.Numerics;
-using System.Reflection.Metadata.Ecma335;
-using System.Text.RegularExpressions;
-using Newtonsoft.Json.Converters;
-
 namespace advent_of_code_csharp_2024;
 
 public class Day01
 {
-    private readonly string _inputFilename = @"../../../Day01_input.txt";
+    public static readonly string InputFilename = @"../../../Day01_input.txt";
 
     private readonly string _testLines = @"3   4
 4   3
@@ -17,15 +11,15 @@ public class Day01
 3   9
 3   3";
 
-    private record Line(int First, int Second);
+    public record Line(int First, int Second);
 
-    private static Line ParseLine(string line){
+    public static Line ParseLine(string line){
         var split = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
 
         return new Line(int.Parse(split[0]), int.Parse(split[1]));
     }
 
-    private static int SolveDay1_Part1(IEnumerable<Line> lines){
+    public static int SolveDay1_Part1(IEnumerable<Line> lines){
         var leftOrdered = lines.OrderBy(x=>x.First);
         var rightOrdered = lines.OrderBy(x=>x.Second);
 
@@ -34,7 +28,7 @@ public class Day01
                 .Sum();
     }
 
-    private static int SolveDay1_Part2(IEnumerable<Line> parsedLines)
+    public static int SolveDay1_Part2(IEnumerable<Line> parsedLines)
     {
         var list1 = parsedLines.Select(_ => _.First);
         var list2 = parsedLines.Select(_ => _.Second);
@@ -56,62 +50,6 @@ public class Day01
                 }
             }).Sum();
         return answer;
-    }
-
-    [Fact]
-    public void ExampleLines_Part1()
-    {
-        var lines = _testLines.Split(System.Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
-
-        var parsedLines = lines
-            .Select(ParseLine);
-
-        Assert.Equal(new Line(3,4), parsedLines.First());
-
-        var ans = SolveDay1_Part1(parsedLines);
-
-        Assert.Equal(11, ans);
-    }
-
-    [Fact]
-    public void RealData_Part1()
-    {
-        var lines = File.ReadLines(_inputFilename);
-
-        var parsedLines = lines
-            .Select(ParseLine);
-
-        var ans = SolveDay1_Part1(parsedLines);
-
-        Assert.Equal(1938424, ans);
-    }
-
-    [Fact]
-    public void ExampleData_Part2()
-    {
-        var lines = _testLines.Split(System.Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
-
-        var parsedLines = lines
-            .Select(ParseLine);
-
-        Assert.Equal(new Line(3, 4), parsedLines.First());
-
-        int answer = SolveDay1_Part2(parsedLines);
-
-        Assert.Equal(31, answer);
-    }
-
-   [Fact]
-    public void RealData_Part2()
-    {
-        var lines = File.ReadLines(_inputFilename);
-
-        var parsedLines = lines
-            .Select(ParseLine);
-
-        var ans = SolveDay1_Part2(parsedLines);
-
-        Assert.Equal(22014209, ans);
     }
 }
  
---
+++ Day01Tests.cs
@@ -0,0 +1,68 @@
+namespace advent_of_code_csharp_2024;
+
+public class Day01Tests
+{
+    private readonly string _testLines = @"3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+
+    [Fact]
+    public void ExampleLines_Part1()
+    {
+        var lines = _testLines.Split(System.Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+        var parsedLines = lines
+            .Select(Day01.ParseLine);
+
+        Assert.Equal(new Day01.Line(3,4), parsedLines.First());
+
+        var ans = Day01.SolveDay1_Part1(parsedLines);
+
+        Assert.Equal(11, ans);
+    }
+
+    [Fact]
+    public void RealData_Part1()
+    {
+        var lines = File.ReadLines(Day01.InputFilename);
+
+        var parsedLines = lines
+            .Select(Day01.ParseLine);
+
+        var ans = Day01.SolveDay1_Part1(parsedLines);
+
+        Assert.Equal(1938424, ans);
+    }
+
+    [Fact]
+    public void ExampleData_Part2()
+    {
+        var lines = _testLines.Split(System.Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+        var parsedLines = lines
+            .Select(Day01.ParseLine);
+
+        Assert.Equal(new Day01.Line(3, 4), parsedLines.First());
+
+        int answer = Day01.SolveDay1_Part2(parsedLines);
+
+        Assert.Equal(31, answer);
+    }
+
+   [Fact]
+    public void RealData_Part2()
+    {
+        var lines = File.ReadLines(Day01.InputFilename);
+
+        var parsedLines = lines
+            .Select(Day01.ParseLine);
+
+        var ans = Day01.SolveDay1_Part2(parsedLines);
+
+        Assert.Equal(22014209, ans);
+    }
+}
+ 
