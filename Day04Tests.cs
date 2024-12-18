f00f289de25af77d63f032c00ff6aaa1f94ba112
adampridmore <320922+adampridmore@users.noreply.github.com>
2024-12-18 13:56:42 +0000

    Solved day4 Part I - Real data

--- Day04Tests.cs
+++ Day04Tests.cs
@@ -1,7 +1,5 @@
-using System.Reflection;
+using System.ComponentModel.Design;
 using System.Runtime.CompilerServices;
-using System.Runtime.ExceptionServices;
-using System.Threading.Channels;
 
 namespace advent_of_code_csharp_2024;
 
@@ -19,94 +17,186 @@ MAMMMXMMMM
 MXMXAXMASX";
 
 
-/*
-.SAMXMS...
-...S..A...
-..A.A.MS.X
-XMASAMX.MM
-X.....XA.A
-S.S.S.S.SS
-.A.A.A.A.A
-..M.M.M.MM
-.X.X.XMASX
-*/
+  /*
+  .SAMXMS...
+  ...S..A...
+  ..A.A.MS.X
+  XMASAMX.MM
+  X.....XA.A
+  S.S.S.S.SS
+  .A.A.A.A.A
+  ..M.M.M.MM
+  .X.X.XMASX
+  */
 
   public static readonly string InputFilename = @"../../../Day04_input.txt";
 
-  public static char GetCellValue(char[][] grid, int x, int y)
+  public record Position(int x, int y);
+
+  public static char GetCellValue(char[][] grid, Position position)
   {
-    if (x < 0 || x >= grid[0].Length || y < 0 || y >= grid.Length){
+    if (position.x < 0 || position.x >= grid[0].Length || position.y < 0 || position.y >= grid.Length)
+    {
       return ' ';
     }
 
-    return grid[y][x];
+    try
+    {
+      return grid[position.y][position.x];
+    }
+    catch (IndexOutOfRangeException ex)
+    {
+      throw new Exception($"Position: {position} {grid[0].Length}, {grid.Length}", ex);
+    }
+  }
+
+  private static bool CheckXmasInDirection(char[][] grid, Position position, Func<Position, Position> positionModifier)
+  {
+    if (GetCellValue(grid, position) != 'X')
+    {
+      return false;
+    }
+
+    var MPos = positionModifier(position);
+    if (GetCellValue(grid, MPos) != 'M')
+    {
+      return false;
+    }
+
+    var APos = positionModifier(MPos);
+    if (GetCellValue(grid, APos) != 'A')
+    {
+      return false;
+    }
+
+    var SPos = positionModifier(APos);
+    if (GetCellValue(grid, SPos) != 'S')
+    {
+      return false;
+    }
+
+    return true;
+  }
+
+  private static int XmasCellCount(char[][] grid, Position p)
+  {
+    bool[] results = [
+      CheckXmasInDirection(grid, p, p => new Position(p.x + 1 , p.y     )), // Right   - OK
+      CheckXmasInDirection(grid, p, p => new Position(p.x + 1 , p.y + 1 )), // Down and Right - OK
+      CheckXmasInDirection(grid, p, p => new Position(p.x,      p.y + 1 )), // Down
+      CheckXmasInDirection(grid, p, p => new Position(p.x - 1 , p.y + 1 )), // Down and left
+      CheckXmasInDirection(grid, p, p => new Position(p.x - 1 , p.y     )), // left
+      CheckXmasInDirection(grid, p, p => new Position(p.x - 1 , p.y - 1 )), // left and up
+      CheckXmasInDirection(grid, p, p => new Position(p.x     , p.y - 1 )), // up
+      CheckXmasInDirection(grid, p, p => new Position(p.x +1  , p.y - 1 )), // up and right
+    ];
+
+    var count = results.Count(r => r);
+    
+    // if (count >0) System.Console.Out.WriteLine($"{p}: {count}");
+    
+    return count;
   }
 
   [Fact]
-  public void GetCellValueTest(){
+  public void GetCellValueTest()
+  {
     char[][] cells = [
       ['A','B'],
       ['C','D']
     ];
 
-    Assert.Equal('A', GetCellValue(cells,0,0));
-    Assert.Equal('B', GetCellValue(cells,1,0));
-    Assert.Equal('C', GetCellValue(cells,0,1));
-    Assert.Equal('D', GetCellValue(cells,1,1));
+    Assert.Equal('A', GetCellValue(cells, new Position(0, 0)));
+    Assert.Equal('B', GetCellValue(cells, new Position(1, 0)));
+    Assert.Equal('C', GetCellValue(cells, new Position(0, 1)));
+    Assert.Equal('D', GetCellValue(cells, new Position(1, 1)));
 
-    Assert.Equal(' ', GetCellValue(cells,-1,0));
-    Assert.Equal(' ', GetCellValue(cells,0,-1));
-    Assert.Equal(' ', GetCellValue(cells,2,0));
-    Assert.Equal(' ', GetCellValue(cells,0,2));
+    Assert.Equal(' ', GetCellValue(cells, new Position(-1, 0)));
+    Assert.Equal(' ', GetCellValue(cells, new Position(0, -1)));
+    Assert.Equal(' ', GetCellValue(cells, new Position(2, 0)));
+    Assert.Equal(' ', GetCellValue(cells, new Position(0, 2)));
   }
 
-  record Position(int x, int y);
-
-  [Fact(Skip = "In progress")]
+  [Fact]
   public void TestExample_part1()
   {
-    char[][] grid = 
-      _testInput
-        .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
-        .Select(line => line.ToCharArray())
-        .ToArray();
+    char[][] grid = LoadGrid();
 
-    // bool CheckXmasInDirection(int x, int y, Func<int,int,<int,int>> positionModifier){
-    //   if (GetCellValue(grid, x, y ) != 'X'){
-    //     return false;
-    //   }
-
-    //   var positionModifier
-    // };
-
-    bool IsXmasCell(char[][] grid, int y, int x){
-      if (GetCellValue(grid, x, y) !='X'){
-        return false;
-      }
-
-      if (GetCellValue(grid, x+1, y) !='M'){
-        return false;
-      }
-
-      if (GetCellValue(grid, x+2, y) !='A'){
-        return false;
-      }
-
-      if (GetCellValue(grid, x+3, y) !='S'){
-        return false;
-      }
-      return true;
-    }
+    // var results = new int[10][];
+    // for(int i = 0 ; i < results.Length;i++){
+    //   results[i] = new int[10];
+    //   Array.Fill(results[i], 0);
+    // }
 
     var count = 0;
-    for(var y = 0; y < grid.Length; y++){
-      for(var x = 0; x < grid[0].Length;x ++){
-        if (IsXmasCell(grid, y, x)){
-          count++;
-        }
+    for (var y = 0; y < grid.Length; y++)
+    {
+      for (var x = 0; x < grid[0].Length; x++)
+      {
+        var cellCount = XmasCellCount(grid, new Position(x, y));
+        count += cellCount;
+        // results[y][x] = cellCount;
       }
     }
 
-    Assert.Equal(2, count);
+    // foreach(var r in results){
+    //   var line = String.Join("", r.Select(r => r.ToString()));
+    //   System.Console.Out.WriteLine(line);
+    // }
+
+    Assert.Equal(18, count);
+  }
+
+   [Fact]
+  public void TestRealData_part1()
+  {
+    char[][] grid = File.ReadLines(InputFilename)
+        .Select(line => line.ToCharArray().Where(c => !char.IsWhiteSpace(c)).ToArray())
+        .ToArray();
+
+    // var results = new int[10][];
+    // for(int i = 0 ; i < results.Length;i++){
+    //   results[i] = new int[10];
+    //   Array.Fill(results[i], 0);
+    // }
+
+    var count = 0;
+    for (var y = 0; y < grid.Length; y++)
+    {
+      for (var x = 0; x < grid[0].Length; x++)
+      {
+        var cellCount = XmasCellCount(grid, new Position(x, y));
+        count += cellCount;
+        // results[y][x] = cellCount;
+      }
+    }
+
+    // foreach(var r in results){
+    //   var line = String.Join("", r.Select(r => r.ToString()));
+    //   System.Console.Out.WriteLine(line);
+    // }
+
+    Assert.Equal(2613, count);
+  }
+
+    private static char[][] LoadGrid()
+    {
+      return _testInput
+        .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+        .Select(line => line.ToCharArray().Where(c => !char.IsWhiteSpace(c)).ToArray())
+        .ToArray();
+    }
+
+  [Fact]
+  public void XmaxCellCountTest(){
+    var grid = LoadGrid();
+    Assert.Equal(0, XmasCellCount(grid, new Position(0,0)));
+
+    Assert.Equal(1, XmasCellCount(grid, new Position(4,0)));
+    Assert.Equal(1, XmasCellCount(grid, new Position(5,0)));
+    Assert.Equal(2, XmasCellCount(grid, new Position(6,4)));
+    Assert.Equal(2, XmasCellCount(grid, new Position(6,4)));
+
+    Assert.Equal(3, XmasCellCount(grid, new Position(5,9)));
   }
 }
