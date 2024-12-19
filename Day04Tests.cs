082789da35ee272ebdcadd979f37b1cc13786943
adampridmore <320922+adampridmore@users.noreply.github.com>
2024-12-19 08:15:39 +0000

    Refactoring

--- Day04Tests.cs
+++ Day04Tests.cs
@@ -13,7 +13,6 @@ SAXAMASAAA
 MAMMMXMMMM
 MXMXAXMASX";
 
-
   /*
   .SAMXMS...
   ...S..A...
@@ -28,76 +27,64 @@ MXMXAXMASX";
 
   public static readonly string InputFilename = @"../../../Day04_input.txt";
 
-  public record Position(int x, int y);
+  public record Position(int X, int Y);
 
   public static char GetCellValue(char[][] grid, Position position)
   {
-    if (position.x < 0 || position.x >= grid[0].Length || position.y < 0 || position.y >= grid.Length)
+    if (
+      position.X < 0 || 
+      position.X >= grid[0].Length || 
+      position.Y < 0 || 
+      position.Y >= grid.Length)
     {
       return ' ';
     }
 
-    return grid[position.y][position.x];
+    return grid[position.Y][position.X];
   }
 
-  private static bool CheckXmasInDirection(char[][] grid, Position position, Func<Position, Position> positionModifier)
+  private static bool CheckXmasInDirection(char[][] grid, Position position, Func<Position, int, Position> positionModifier)
   {
-    if (GetCellValue(grid, position) != 'X')
-    {
-      return false;
-    }
+    var toMatch = new List<(char, Position)> {
+      ('X', positionModifier(position,0)),
+      ('M', positionModifier(position,1)),
+      ('A', positionModifier(position,2)),
+      ('S', positionModifier(position,3))
+    };
 
-    var MPos = positionModifier(position);
-    if (GetCellValue(grid, MPos) != 'M')
-    {
-      return false;
-    }
-
-    var APos = positionModifier(MPos);
-    if (GetCellValue(grid, APos) != 'A')
-    {
-      return false;
-    }
-
-    var SPos = positionModifier(APos);
-    if (GetCellValue(grid, SPos) != 'S')
-    {
-      return false;
-    }
-
-    return true;
+    return !toMatch.Any(match=>{
+      var letter = match.Item1;
+      var p = match.Item2;
+      return GetCellValue(grid, p) != letter;
+    });
   }
 
   private static int XmasCellCount(char[][] grid, Position p)
   {
     bool[] results = [
-      CheckXmasInDirection(grid, p, p => new Position(p.x + 1 , p.y     )), // Right   - OK
-      CheckXmasInDirection(grid, p, p => new Position(p.x + 1 , p.y + 1 )), // Down and Right - OK
-      CheckXmasInDirection(grid, p, p => new Position(p.x,      p.y + 1 )), // Down
-      CheckXmasInDirection(grid, p, p => new Position(p.x - 1 , p.y + 1 )), // Down and left
-      CheckXmasInDirection(grid, p, p => new Position(p.x - 1 , p.y     )), // left
-      CheckXmasInDirection(grid, p, p => new Position(p.x - 1 , p.y - 1 )), // left and up
-      CheckXmasInDirection(grid, p, p => new Position(p.x     , p.y - 1 )), // up
-      CheckXmasInDirection(grid, p, p => new Position(p.x +1  , p.y - 1 )), // up and right
+      CheckXmasInDirection(grid, p, (p,distance) => new Position(p.X + distance , p.Y            )), // Right   - OK
+      CheckXmasInDirection(grid, p, (p,distance) => new Position(p.X + distance , p.Y + distance )), // Down and Right - OK
+      CheckXmasInDirection(grid, p, (p,distance) => new Position(p.X            , p.Y + distance )), // Down
+      CheckXmasInDirection(grid, p, (p,distance) => new Position(p.X - distance , p.Y + distance )), // Down and left
+      CheckXmasInDirection(grid, p, (p,distance) => new Position(p.X - distance , p.Y            )), // left
+      CheckXmasInDirection(grid, p, (p,distance) => new Position(p.X - distance , p.Y - distance )), // left and up
+      CheckXmasInDirection(grid, p, (p,distance) => new Position(p.X            , p.Y - distance )), // up
+      CheckXmasInDirection(grid, p, (p,distance) => new Position(p.X + distance , p.Y - distance )), // up and right
     ];
 
-    var count = results.Count(r => r);
-    
-    return count;
+    return results.Count(r => r);
   }
 
   private static int CountXmas(char[][] grid)
     {
-        var count = 0;
-        for (var y = 0; y < grid.Length; y++)
-        {
-            for (var x = 0; x < grid[0].Length; x++)
-            {
-                count += XmasCellCount(grid, new Position(x, y)); ;
-            }
-        }
+      var count = 
+        grid.Select((line, yIndex) => {
+          return line.Select((cell, xIndex) => {
+            return XmasCellCount(grid, new Position(xIndex,yIndex));
+          }).Sum();
+        }).Sum();
 
-        return count;
+      return count;
     }
 
   [Fact]
